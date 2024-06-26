e309a9b7778c82b0972ab011d679233d66e5a2e6
Bruno Marmello <brmarmello@gmail.com>
2024-06-26 15:53:26 -0300

    Create Function 'main' and finish challenge V2.

--- 02 - Programação Orientada a Objetos/10 - Desafios/desafio_v2.py
+++ 02 - Programação Orientada a Objetos/10 - Desafios/desafio_v2.py
@@ -306,3 +306,37 @@ def listar_contas(contas):
         print(textwrap.dedent(str(conta)))
 
 
+def main():
+    clientes = []
+    contas = []
+
+    while True:
+        opcao = menu()
+
+        if opcao == "d":
+            depositar(clientes)
+
+        elif opcao == "s":
+            sacar(clientes)
+
+        elif opcao == "e":
+            exibir_extrato(clientes)
+
+        elif opcao == "nu":
+            criar_cliente(clientes)
+
+        elif opcao == "nc":
+            numero_conta = len(contas) + 1
+            criar_conta(numero_conta, clientes, contas)
+
+        elif opcao == "lc":
+            listar_contas(contas)
+
+        elif opcao == "q":
+            break
+
+        else:
+            print("\n@@@ Operação inválida, por favor selecione novamente a operação desejada. @@@")
+
+
+main()
