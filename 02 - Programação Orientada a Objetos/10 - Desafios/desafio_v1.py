098e6fa4c610c06c09747334e78add64349457ad
Bruno Marmello <brmarmello@gmail.com>
2024-06-26 15:13:55 -0300

    Create Class 'PessoaFisica'.

--- 02 - Programação Orientada a Objetos/10 - Desafios/desafio_v1.py
+++ 02 - Programação Orientada a Objetos/10 - Desafios/desafio_v1.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractclassmethod, abstractproperty
 from datetime import datetime
 
+
 class Cliente:
     def __init__(self, endereco):
         self.endereco = endereco
@@ -11,5 +12,13 @@ class Cliente:
 
     def adicionar_conta(self, conta):
         self.contas.append(conta)
+
+
+class PessoaFisica(Cliente):
+    def __init__(self, nome, data_nascimento, cpf, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
 
 
