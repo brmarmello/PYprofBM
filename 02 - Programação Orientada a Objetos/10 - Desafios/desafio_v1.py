5f3944c8abd360863131bc2c5d3da1cd9d2c4920
Bruno Marmello <brmarmello@gmail.com>
2024-06-26 15:10:56 -0300

    Create Class 'Customers'.

--- 02 - Programação Orientada a Objetos/10 - Desafios/desafio_v1.py
+++ 02 - Programação Orientada a Objetos/10 - Desafios/desafio_v1.py
@@ -1,2 +1,15 @@
 from abc import ABC, abstractclassmethod, abstractproperty
 from datetime import datetime
+
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
+
+
